1f90e0c75e349afb9a0ed7d26fc5a636c914f0dd
pgogad <pg1099@txstate.edu>
2015-02-28 10:44:26 -0600

    Changed the logo

--- gen/com/advisor/app/R.java
+++ gen/com/advisor/app/R.java
@@ -28,22 +28,11 @@ public final class R {
         public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int alisa=0x7f020000;
-        public static final int alisa_logo=0x7f020001;
-        public static final int alisav=0x7f020002;
-        public static final int drawer_shadow=0x7f020003;
-        public static final int greylogo=0x7f020004;
-        public static final int ic_drawer=0x7f020005;
-        public static final int ic_launcher=0x7f020006;
-        public static final int ic_launcher_1=0x7f020007;
-        public static final int ic_launcher_2=0x7f020008;
-        public static final int ic_launcher_3=0x7f020009;
-        public static final int sian_icon=0x7f02000a;
-        public static final int siane_logo=0x7f02000b;
-        public static final int siane_logo_1=0x7f02000c;
-        public static final int siane_logo_2=0x7f02000d;
-        public static final int splashscreen=0x7f02000e;
-        public static final int text_view=0x7f02000f;
+        public static final int alisav=0x7f020000;
+        public static final int drawer_shadow=0x7f020001;
+        public static final int ic_drawer=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int splashscreen=0x7f020004;
     }
     public static final class id {
         public static final int TextView01=0x7f0a0003;
